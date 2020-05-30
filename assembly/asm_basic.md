34df5ec4456ceb0a5545d5fdd8c535171cb30260
kkkmmu <kkkmmu@163.com>
2020-05-30 09:04:42 -0700

    x86 assembly source code

---
+++ assembly/asm_basic.md
@@ -0,0 +1,6 @@
+1. find /usr/include -name *.h | grep unistd
+	Use this command to find the system call number define on your system. Usually system call defined in unistd_32.h for 32 bit system and unistd_64.h for 64 bit system.
+
+2. man 2 read
+	Use this command to get the programming guide for read system call.
+
---
+++ assembly/hello_world.asm
@@ -0,0 +1,22 @@
+global _start;
+
+section .text:
+
+_start:
+	mov eax, 0x4               ;system call number should be passed with eax
+	mov ebx, 1                 ;first parameter for write. (fd)
+	mov ecx, message           ;second parameter for write. (buf)
+	mov edx, message_length    ;thired parameter for write. (buf_len)
+	int 0x80                   ;do system call
+
+	mov eax, 0x1               ;another system call (exit)
+	mov ebx, 0                 ;parameter
+	int 0x80                   ;do system call
+
+section .data:
+	message: db "Hello World!", 0xA
+	message_length: equ $-message
+
+
+;nasm -f elf64 -o hello_world.o hello_world.asm
+;ld -m elf_x86_64 -o hello_world hello_world.o
